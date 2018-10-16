86c829aaad003919e83aff24c223cde448634392
gabrielfeba <gabrielfeba@outlook.com>
2018-10-16 12:17:44 -0300

    adicionar quarto exercicio com forma lógica do bônus.

--- exercicio4/js/script.js
+++ exercicio4/js/script.js
@@ -1,12 +1,11 @@
-//exercicio 4 sem o bônus
+//exercicio 4 com o bônus
 
-let meninos = ["Alfredo", "Pedro", "Anastácio", "Waldisney"];
-let meninas = ["Alexandrina", "Francisca", "Josefa", "Marinalva"];
+let meninosAndMeninas = ["Alfredo", "Pedro", "Anastácio", "Waldisney","Alexandrina", "Francisca", "Josefa", "Marinalva"];
 let contador = 0;
 
-for(let menino of meninos) {
-    for(let menina of meninas) {
-        console.log(`O ${menino} e a ${menina} formam casal.`);
+for(let i = 0; i < meninosAndMeninas.length; i++) {
+    for(let y = i + 1; y < meninosAndMeninas.length; y++) {
+        console.log(`${meninosAndMeninas[i]} e ${meninosAndMeninas[y]} podem formar casal.`);
         contador++;
     }
 }
