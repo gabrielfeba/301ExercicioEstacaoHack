bffc9ca00f6e64605526bb65017c659e44941494
gabrielfeba <gabrielfeba@outlook.com>
2018-10-16 11:46:07 -0300

    adicionar terceiro exercicio

---
+++ exercicio3/js/script.js
@@ -0,0 +1,9 @@
+let quantidade = prompt("Quantas degraus você quer?");
+let contador = 0;
+let escadinha = "";
+let tipoDeEscadinha = prompt("Qual o tipo de escadinha?");
+
+while(contador < quantidade) {
+    console.log(escadinha += tipoDeEscadinha);
+    contador++;
+}
